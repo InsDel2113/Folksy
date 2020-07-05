d6d54b6c8f3c2068b0d0d924f90e47c0d48c0201
InsDel2113 <52561077+InsDel2113@users.noreply.github.com>
2020-07-04 18:41:42 -0600

    removed my user data lul

--- folksy_framework.sql
+++ folksy_framework.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2020 at 02:41 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -23,18 +32,6 @@ CREATE TABLE `forum_categories` (
   `name` varchar(75) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Truncate table before insert `forum_categories`
---
-
-TRUNCATE TABLE `forum_categories`;
---
--- Dumping data for table `forum_categories`
---
-
-INSERT INTO `forum_categories` (`id`, `name`) VALUES
-(1, 'On-topic');
-
 -- --------------------------------------------------------
 
 --
@@ -52,27 +49,6 @@ CREATE TABLE `forum_posts` (
   `cat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Truncate table before insert `forum_posts`
---
-
-TRUNCATE TABLE `forum_posts`;
---
--- Dumping data for table `forum_posts`
---
-
-INSERT INTO `forum_posts` (`id`, `title`, `content`, `reply`, `threadid`, `poster_id`, `replier_uname`, `cat_id`) VALUES
-(1, 'yes this is a post', 'some content here\r\n\r\nsome content there', 0, 0, 2147483647, '', 0),
-(999, 'test', '', 1, 1, 0, 'InsDel2113', 0),
-(1000, 'testsf', 'testsdfsdfsdf', 0, 0, 2147483647, '', 1),
-(1001, 'testsf', 'testsdfsdfsdf', 0, 0, 2147483647, '', 1),
-(1002, 'sssss', 'ssssss', 0, 0, 2147483647, '', 1),
-(1003, 'test', 'test', 1, 0, 2147483647, 'InsDel2113', 0),
-(1004, 'test', 'test', 1, 1002, 2147483647, 'InsDel2113', 0),
-(1005, 'test', 'test', 1, 1002, 2147483647, 'InsDel2113', 0),
-(1006, 'test', 'test', 1, 1002, 2147483647, 'InsDel2113', 0),
-(1007, 'cool', 'cool', 1, 1002, 2147483647, 'InsDel2113', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -86,18 +62,6 @@ CREATE TABLE `users` (
   `ip` varchar(255) NOT NULL,
   `user_agent` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Truncate table before insert `users`
---
-
-TRUNCATE TABLE `users`;
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userid`, `username`, `password`, `ip`, `user_agent`) VALUES
-(2147483647, 'InsDel2113', '$2y$10$UBs/1k9HafMKEl.q5adVae2Yb9YxObnbi4KHfVOvmKiMhkpxuTyJ2', '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36');
 
 --
 -- Indexes for dumped tables
@@ -129,13 +93,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `forum_categories`
 --
 ALTER TABLE `forum_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `forum_posts`
 --
 ALTER TABLE `forum_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
